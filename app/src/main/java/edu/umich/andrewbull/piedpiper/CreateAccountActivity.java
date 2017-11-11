7d908e1a6d9a7096839b9e835d72083bc9ccbd71
bittkerj <bittkerj@umich.edu>
2017-11-11 16:37:26 -0500

    Create Account page

---
+++ app/src/main/java/edu/umich/andrewbull/piedpiper/CreateAccountActivity.java
@@ -0,0 +1,13 @@
+package edu.umich.andrewbull.piedpiper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
