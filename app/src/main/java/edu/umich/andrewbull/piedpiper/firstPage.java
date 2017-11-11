b71b2733b13d7f2187cb9e597cd24b09b3b2e76f
Richard Liu <rcliu@umich.edu>
2017-11-11 17:34:45 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/edu/umich/andrewbull/piedpiper/firstPage.java
+++ app/src/main/java/edu/umich/andrewbull/piedpiper/firstPage.java
@@ -3,23 +3,33 @@ package edu.umich.andrewbull.piedpiper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class firstPage extends Activity {
+public class firstPage extends Activity implements Button.OnClickListener{
 
-    private ImageButton imageButtonDishPic;
-    private TextView textViewDishName;
+    private ImageButton imageButtonDish1, imageButtonDish2, imageButtonDish3, imageButtonDish4;
+    private TextView textViewDish1, textViewDish2, textViewDish3, textViewDish4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
 
-        imageButtonDishPic = (ImageButton) findViewById(R.id.imageButtonDishPic);
-        textViewDishName = (TextView) findViewById(R.id.textViewDishName);
+        imageButtonDish1 = (ImageButton) findViewById(R.id.imageButtonDish1);
+        imageButtonDish2 = (ImageButton) findViewById(R.id.imageButtonDish2);
+        imageButtonDish3 = (ImageButton) findViewById(R.id.imageButtonDish3);
+        imageButtonDish4 = (ImageButton) findViewById(R.id.imageButtonDish4);
+        textViewDish1 = (TextView) findViewById(R.id.textViewDish1);
+        textViewDish2 = (TextView) findViewById(R.id.textViewDish2);
+        textViewDish3 = (TextView) findViewById(R.id.textViewDish3);
+        textViewDish4 = (TextView) findViewById(R.id.textViewDish4);
 
-        imageButtonDishPic.setOnClickListener(this);
+        imageButtonDish1.setOnClickListener(this);
+        imageButtonDish2.setOnClickListener(this);
+        imageButtonDish3.setOnClickListener(this);
+        imageButtonDish4.setOnClickListener(this);
     }
 
     @Override
