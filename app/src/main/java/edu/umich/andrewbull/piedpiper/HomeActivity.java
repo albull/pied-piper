f12b154062d7ab2a4ae18975c6477b5f20d20bb1
bittkerj <bittkerj@umich.edu>
2017-11-20 18:10:27 -0500

    New Log In page, transfer first page to home page, create account placeholders

--- app/src/main/java/edu/umich/andrewbull/piedpiper/HomeActivity.java
+++ app/src/main/java/edu/umich/andrewbull/piedpiper/HomeActivity.java
@@ -1,17 +1,42 @@
 package edu.umich.andrewbull.piedpiper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Activity implements Button.OnClickListener {
+
+
+    private ImageButton imageButtonDish1, imageButtonDish2, imageButtonDish3, imageButtonDish4;
+    private TextView textViewDish1, textViewDish2, textViewDish3, textViewDish4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //jason change
-        // richard's change
-        //Ryan's changes 123
+
+            imageButtonDish1 = (ImageButton) findViewById(R.id.imageButtonDish1);
+            imageButtonDish2 = (ImageButton) findViewById(R.id.imageButtonDish2);
+            imageButtonDish3 = (ImageButton) findViewById(R.id.imageButtonDish3);
+            imageButtonDish4 = (ImageButton) findViewById(R.id.imageButtonDish4);
+            textViewDish1 = (TextView) findViewById(R.id.textViewDish1);
+            textViewDish2 = (TextView) findViewById(R.id.textViewDish2);
+            textViewDish3 = (TextView) findViewById(R.id.textViewDish3);
+            textViewDish4 = (TextView) findViewById(R.id.textViewDish4);
+
+            imageButtonDish1.setOnClickListener(this);
+            imageButtonDish2.setOnClickListener(this);
+            imageButtonDish3.setOnClickListener(this);
+            imageButtonDish4.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
